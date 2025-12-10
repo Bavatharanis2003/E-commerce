c796cd3e7c317299d662a41afaad008f70406ad8
Bavatharanis2003 <158263641+Bavatharanis2003@users.noreply.github.com>
2025-12-10 12:28:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,134 +1,111 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
-const products =[
+const products = [
     {
-        id:1,
-        title:"Air Force",
-        price:119,
+        id: 1,
+        title: "Air Force",
+        price: 119,
         colors: [
-            {
-                code:"darkblue",
-                img: "./img/air.png",
-            },
-            {
-                code:"blue",
-                img:"./img/air2.png",
-            },
+            { code: "darkblue", img: "./img/air.png" },
+            { code: "blue", img: "./img/air2.png" },
         ],
     },
     {
-        id:2,
-        title:"Air Jordan",
-        price:149,
-        colors:[
-            {
-                code:"white",
-                img:"./img/jordan.png",
-            },
-            {
-                code:"black",
-                img:"./img/jordan2.png",
-             },
-             ],
-        },
-        {
-         id:3,
-         title:"Air Blazer",
-         price:150, 
-         colors:[
-            {
-                code:"black",
-                img:"./img/blazer.png",
-            },
-            {  
-                code:"gray with wite",
-                img:"./img/blazer2.png",
-             },
-         ],
-        },
-        {
-            id:4,
-            title:"Air crater",
-            price:180,
-            colors:[
-                {
-                    code:"gray",
-                    img:"./img/crater.png",
-                },
-                {
-                    code:"blue",
-                    img:"./img/crater2.png",
-                },
-            ],
-        },
-        {
-            id:5,
-            title:"Air Hippie",
-            price:200,
-            colors:[
-                {
-                    code:"blue",
-                    img:"./img/Hippie.png",
-                },
-                {
-                    code:"neon yellow",
-                    img:"./img/hippie2.png",
-                },
-            ],
-        },
+        id: 2,
+        title: "Air Jordan",
+        price: 149,
+        colors: [
+            { code: "white", img: "./img/jordan.png" },
+            { code: "black", img: "./img/jordan2.png" },
+        ],
+    },
+    {
+        id: 3,
+        title: "Air Blazer",
+        price: 150,
+        colors: [
+            { code: "black", img: "./img/blazer.png" },
+            { code: "gray", img: "./img/blazer2.png" },
+        ],
+    },
+    {
+        id: 4,
+        title: "Air Crater",
+        price: 180,
+        colors: [
+            { code: "gray", img: "./img/crater.png" },
+            { code: "blue", img: "./img/crater2.png" },
+        ],
+    },
+    {
+        id: 5,
+        title: "Air Hippie",
+        price: 200,
+        colors: [
+            { code: "blue", img: "./img/hippie.png" },  // make sure filename is lowercase!
+            { code: "yellow", img: "./img/hippie2.png" },
+        ],
+    },
 ];
 
-let choosenProduct = products[0]
+let choosenProduct = products[0];
 
 const currentproductImg = document.querySelector(".productImg");
 const currentproductTitle = document.querySelector(".productTitle");
 const currentproductPrice = document.querySelector(".productPrice");
 const currentproductColors = document.querySelectorAll(".color");
-const currentproductSizes = document.querySelectorAll(".size")
+const currentproductSizes = document.querySelectorAll(".size");
 
-menuItems.forEach((item,index) => {
-    item.addEventListenner("click",() => {
-        //change the current slide
-        wrapper.style.transform = 'translateX(${-100 * index}vw)';
+// SLIDER CLICK
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
 
-        //change the choosen product
-        choosenProduct = products[index]
+        // Slide effect
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
 
-        //change texts of currentproduct
+        // Change product
+        choosenProduct = products[index];
+
+        // Update details
         currentproductTitle.textContent = choosenProduct.title;
         currentproductPrice.textContent = "$" + choosenProduct.price;
-        currentproductImg.src = choosenProduct.colors[0].img
+        currentproductImg.src = choosenProduct.colors[0].img;
 
-        currentproductColors.forEach((colors,index)=>{
-            colors.style.backgroundColor = choosenProduct.colors[index].code;
-         });
+        // Update colors
+        currentproductColors.forEach((color, idx) => {
+            color.style.backgroundColor = choosenProduct.colors[idx].code;
+        });
     });
 });
 
-
-currentproductColors.forEach((colors,index) => {
-    colors.addEventListener("click",() => {
+// CHANGE IMAGE ON COLOR CLICK
+currentproductColors.forEach((color, index) => {
+    color.addEventListener("click", () => {
         currentproductImg.src = choosenProduct.colors[index].img;
     });
 });
 
-currentproductSizes.forEach((size,index) => {
+// SIZE SELECTOR
+currentproductSizes.forEach((size, index) => {
     size.addEventListener("click", () => {
-        currentproductSizes.forEach((size) => {
-            size.style.backgroundColor="white";
-            size.style.colors="black";
-        });
-        size.style.backgroundColor="black"
-        size.style.colors="white"
-    });
-});  
 
+        currentproductSizes.forEach((s) => {
+            s.style.backgroundColor = "white";
+            s.style.color = "black";
+        });
+
+        size.style.backgroundColor = "black";
+        size.style.color = "white";
+    });
+});
+
+// PAYMENT POP-UP
 const productButton = document.querySelector(".productButton");
 const payment = document.querySelector(".payment");
 const close = document.querySelector(".close");
 
-productButton.addEventListenner("click",() =>{
+productButton.addEventListener("click", () => {
     payment.style.display = "flex";
 });
 
